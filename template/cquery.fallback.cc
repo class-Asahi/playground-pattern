fc589cdd0cf8e00a91ce6247f824ef985b99f661
白羽七海 <ymd45921@163.com>
2021-04-22 12:59:11 +0800

    Update cquery.fallback.cc
    为什么 `operator,` 的重载不支持 `std::ostream &(*)(std::ostream &)` 啊（

--- template/cquery.fallback.cc
+++ template/cquery.fallback.cc
@@ -4,6 +4,7 @@ struct {
 } input;
 
 struct {
+    auto &operator,(std::ostream &(*x)(std::ostream &)) { return std::cout << x, *this; }
     template <class token>
     auto &operator,(token &x) { return std::cout << x, *this; }
     template <class token>
